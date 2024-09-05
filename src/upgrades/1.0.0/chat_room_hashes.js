9d5aa039968e15982e2b29f2ca70236d7431ed3b
NickDevi <ndevidze@andrew.cmu.edu>
2024-09-05 19:30:53 +0300

    AddingConsoleLogs

--- src/upgrades/1.0.0/chat_room_hashes.js
+++ src/upgrades/1.0.0/chat_room_hashes.js
@@ -7,6 +7,7 @@ module.exports = {
 	name: 'Chat room hashes',
 	timestamp: Date.UTC(2015, 11, 23),
 	method: function (callback) {
+		console.log('ndevidze');
 		db.getObjectField('global', 'nextChatRoomId', (err, nextChatRoomId) => {
 			if (err) {
 				return callback(err);
@@ -27,10 +28,12 @@ module.exports = {
 };
 
 function shouldProcessNextRoom(currentChatRoomId, nextChatRoomId, next) {
+	console.log('ndevidze');
 	next(null, currentChatRoomId <= nextChatRoomId);
 }
 
 function processChatRoom(currentChatRoomId, next, incrementCurrentChatRoom) {
+	console.log('ndevidze');
 	getRoomUids(currentChatRoomId, (err, uids) => {
 		if (err) {
 			return next(err);
@@ -52,13 +55,16 @@ function processChatRoom(currentChatRoomId, next, incrementCurrentChatRoom) {
 }
 
 function getRoomUids(currentChatRoomId, callback) {
+	console.log('ndevidze');
 	db.getSortedSetRange(`chat:room:${currentChatRoomId}:uids`, 0, 0, callback);
 }
 
 function isValidUids(uids) {
+	console.log('ndevidze');
 	return Array.isArray(uids) && uids.length && uids[0];
 }
 
 function setChatRoomOwner(roomId, ownerUid, callback) {
+	console.log('ndevidze');
 	db.setObject(`chat:room:${roomId}`, { owner: ownerUid, roomId }, callback);
 }
