b51b6087a6fcf5783ad3ea62b2899ba3565522fe
NickDevi <ndevidze@andrew.cmu.edu>
2024-09-05 18:02:27 +0300

    ReducingDepthToThree

--- src/upgrades/1.0.0/chat_room_hashes.js
+++ src/upgrades/1.0.0/chat_room_hashes.js
@@ -3,7 +3,6 @@
 const async = require('async');
 const db = require('../../database');
 
-
 module.exports = {
 	name: 'Chat room hashes',
 	timestamp: Date.UTC(2015, 11, 23),
@@ -12,28 +11,37 @@ module.exports = {
 			if (err) {
 				return callback(err);
 			}
-			let currentChatRoomId = 1;
-			async.whilst((next) => {
-				next(null, currentChatRoomId <= nextChatRoomId);
-			}, (next) => {
-				db.getSortedSetRange(`chat:room:${currentChatRoomId}:uids`, 0, 0, (err, uids) => {
-					if (err) {
-						return next(err);
-					}
-					if (!Array.isArray(uids) || !uids.length || !uids[0]) {
-						currentChatRoomId += 1;
-						return next();
-					}
-
-					db.setObject(`chat:room:${currentChatRoomId}`, { owner: uids[0], roomId: currentChatRoomId }, (err) => {
-						if (err) {
-							return next(err);
-						}
-						currentChatRoomId += 1;
-						next();
-					});
-				});
-			}, callback);
+			processChatRooms(1, nextChatRoomId, callback);
 		});
 	},
 };
+
+function processChatRooms(currentChatRoomId, nextChatRoomId, callback) {
+	async.whilst(
+		(next) => next(null, currentChatRoomId <= nextChatRoomId),
+		(next) => processSingleChatRoom(currentChatRoomId++, next),
+		callback
+	);
+}
+
+function processSingleChatRoom(chatRoomId, next) {
+	getFirstUserFromChatRoom(chatRoomId, (err, firstUser) => {
+		if (err || !firstUser) {
+			return next(err); // Continue on error or if no user found
+		}
+		setChatRoomOwner(chatRoomId, firstUser, next);
+	});
+}
+
+function getFirstUserFromChatRoom(chatRoomId, callback) {
+	db.getSortedSetRange(`chat:room:${chatRoomId}:uids`, 0, 0, (err, uids) => {
+		if (err) {
+			return callback(err);
+		}
+		callback(null, Array.isArray(uids) && uids.length && uids[0] ? uids[0] : null);
+	});
+}
+
+function setChatRoomOwner(chatRoomId, ownerId, callback) {
+	db.setObject(`chat:room:${chatRoomId}`, { owner: ownerId, roomId: chatRoomId }, callback);
+}
